28a75c648283eff6512523ca5457baf118d98b42
batos1er <baptiste.menetrat@iut-dijon.u-bourgogne.fr>
2023-03-28 16:27:48 +0200

    Create BookTest.java

---
+++ book/src/test/java/livre/BookTest.java
@@ -0,0 +1,29 @@
+package livre;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
+
+class BookTest {
+
+    @MethodSource
+    public void BookTestGetter() {
+        Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925");
+        assertEquals("The Great Gatsby", book.getTitre());
+        assertEquals("F. Scott Fitzgerald", book.getAuteur());
+        assertEquals("1925", book.getDate());
+    }
+
+    @MethodSource
+    public void testSetters() {
+        Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", "1925");
+        book.setTitre("To Kill a Mockingbird");
+        book.setAuteur("Harper Lee");
+        book.setDate("1960");
+        assertEquals("To Kill a Mockingbird", book.getTitre());
+        assertEquals("Harper Lee", book.getAuteur());
+        assertEquals("1960", book.getDate());
+    }
+}
